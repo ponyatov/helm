063733b511739466f76d7ca453f9cae7bd1067d5
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-19 00:48:02 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/Main.elm
+++ src/Main.elm
@@ -2,9 +2,9 @@ module Main exposing (main)
 
 import Browser
 import Debug exposing (log)
-import Html exposing (button, div, img, input, node, text)
+import Html exposing (a, button, div, img, input, node, p, text)
 import Html.Attributes exposing (href, id, placeholder, rel, required, src)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 import String exposing (fromInt)
 
 
@@ -14,6 +14,7 @@ init =
 
 type Message
     = Add
+    | TextChanged String
 
 
 update msg model =
@@ -25,24 +26,37 @@ update msg model =
         Add ->
             let
                 _ =
-                    log "update" msg
+                    log "Add" msg
             in
             { model | value = model.value + 1 }
+
+        TextChanged s ->
+            let
+                _ =
+                    log "TextChanged" s
+            in
+            { model | name = s }
 
 
 css path =
     node "link" [ rel "stylesheet", href path ] []
 
 
+tutorial =
+    div [] [ a [ href "https://www.youtube.com/watch?v=tsSG7aAp_z8&list=PL9l1zUfnZkZlzD2JAAnezqyPIoRnhJWzI&index=7" ] [ text "tutorial" ] ]
+
+
 view model =
     div
         []
         [ css "/css.css"
-        , text (fromInt model.value)
-        , div [] []
-        , input [ id "val", placeholder model.name, required True ] []
+        , text (fromInt model.value ++ ": ")
+        , div [] [ text model.name ]
+        , input [ id "val", placeholder model.name, required True, onInput TextChanged ] []
         , button [ onClick Add ] [ text "Add" ]
         , img [ id "biglogo", src "control.png" ] []
+        , p [] []
+        , tutorial
         ]
 
 
