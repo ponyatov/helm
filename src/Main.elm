5974559c5f509ed899e7c6a0e8ad0afd08c60bf8
Dmitry Ponyatov <ponyatov@example.com>
2024-07-18 18:34:37 +0400

    Signed-off-by: Dmitry Ponyatov <ponyatov@example.com>

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,41 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (div, text)
+import Html exposing (button, div, input, text)
+import Html.Attributes exposing (id, placeholder, required)
+import Html.Events exposing (onClick)
+import String exposing (fromInt)
 
 
 add a b =
     a + b
 
 
+init =
+    { value = 0, name = "Kris" }
+
+
+type Message
+    = Add
+
+
+update msg model =
+    case msg of
+        Add ->
+            { model | value = model.value + 1 }
+
+
+view model =
+    div []
+        [ text (fromInt model.value)
+        , div [] []
+        , input [ id "val", placeholder model.name, required True ] []
+        , button [ onClick Add ] [ text "Add" ]
+        ]
+
+
 main =
-    text "Hello"
+    Browser.sandbox { init = init, view = view, update = update }
 
 
 
